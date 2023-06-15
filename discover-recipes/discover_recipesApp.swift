c6f476868bc584fd73fe5a1b7e107a85dc4db649
Jorge Morin <jmorin1@hawk.iit.edu>
2023-06-15 13:47:21 -0500

    Initial Commit

---
+++ discover-recipes/discover_recipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  discover_recipesApp.swift
+//  discover-recipes
+//
+//  Created by Claudia Morin on 6/15/23.
+//
+
+import SwiftUI
+
+@main
+struct discover_recipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

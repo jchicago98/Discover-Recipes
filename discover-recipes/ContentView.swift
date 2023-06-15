f0457c8e64661c4e2c70ae4a97347276c71d4ceb
Jorge Morin <jmorin1@hawk.iit.edu>
2023-06-15 15:55:48 -0500

    Create a template so that it can be able to dynamically change data to show dessert recipes

--- discover-recipes/ContentView.swift
+++ discover-recipes/ContentView.swift
@@ -2,15 +2,71 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                ForEach(dummyRecipes) { recipe in
+                    NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
+                        RecipeCell(recipe: recipe)
+                    }
+                }
+            }
+            .navigationTitle("Discover Recipes")
         }
-        .padding()
     }
 }
+
+struct RecipeCell: View {
+    var recipe: Recipe
+
+    var body: some View {
+        HStack {
+            Image(recipe.imageName)
+                .resizable()
+                .frame(width: 80, height: 80)
+                .cornerRadius(8)
+            VStack(alignment: .leading) {
+                Text(recipe.name)
+                    .font(.headline)
+                Text(recipe.category)
+                    .font(.subheadline)
+            }
+        }
+    }
+}
+
+struct RecipeDetailView: View {
+    var recipe: Recipe
+
+    var body: some View {
+        VStack {
+            Image(recipe.imageName)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+            Text(recipe.name)
+                .font(.title)
+                .padding()
+            Text(recipe.instructions)
+                .multilineTextAlignment(.leading)
+                .padding()
+            Spacer()
+        }
+        .navigationTitle(recipe.name)
+    }
+}
+
+struct Recipe: Identifiable {
+    let id = UUID()
+    let name: String
+    let category: String
+    let imageName: String
+    let instructions: String
+}
+
+let dummyRecipes: [Recipe] = [
+    Recipe(name: "Apple Pie", category: "Dessert", imageName: "apple_pie", instructions: "This is how you make an apple pie"),
+    Recipe(name: "Chocolate Cake", category: "Dessert", imageName: "chocolate_cake", instructions: "This is how you make a chocolate cake"),
+    Recipe(name: "Vanilla Cupcakes", category: "Dessert", imageName: "vanilla_cupcakes", instructions: "This is how you make vanilla cupcakes")
+]
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

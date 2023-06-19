22bc01058f757bb1f6943e19cfb11322d4bfaf38
Jorge Morin <jmorin1@hawk.iit.edu>
2023-06-19 11:54:09 -0500

    Fetch first API endpoint to display list of desserts, users can now see desserts in alphabetical order and navigate to recipe detail view

--- discover-recipes/ContentView.swift
+++ discover-recipes/ContentView.swift
@@ -1,51 +1,101 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var meals: [Meal] = []
+    
     var body: some View {
         NavigationView {
             List {
-                ForEach(dummyRecipes) { recipe in
-                    NavigationLink(destination: RecipeDetailView(recipe: recipe)) {
-                        RecipeCell(recipe: recipe)
+                ForEach(meals) { meal in
+                    NavigationLink(destination: RecipeDetailView(recipe: meal)) {
+                        RecipeCell(recipe: meal)
                     }
                 }
             }
             .navigationTitle("Discover Recipes")
+            .onAppear {
+                fetchMeals()
+            }
         }
+    }
+    
+    private func fetchMeals() {
+        let url = URL(string: "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert")!
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                print("Error: \(error)")
+                return
+            }
+            
+            guard let data = data else {
+                print("No data received")
+                return
+            }
+            
+            do {
+                let response = try JSONDecoder().decode(MealsResponse.self, from: data)
+                DispatchQueue.main.async {
+                    self.meals = response.meals
+                }
+            } catch {
+                print("Error decoding JSON: \(error)")
+            }
+        }.resume()
     }
 }
 
 struct RecipeCell: View {
-    var recipe: Recipe
-
+    var recipe: Meal
+    
     var body: some View {
         HStack {
-            Image(recipe.imageName)
-                .resizable()
-                .frame(width: 80, height: 80)
-                .cornerRadius(8)
+            if let imageUrl = URL(string: recipe.image) {
+                AsyncImage(url: imageUrl) { image in
+                    image
+                        .resizable()
+                        .frame(width: 80, height: 80)
+                        .cornerRadius(8)
+                } placeholder: {
+                    Color.gray
+                        .frame(width: 80, height: 80)
+                        .cornerRadius(8)
+                }
+            } else {
+                Color.gray
+                    .frame(width: 80, height: 80)
+                    .cornerRadius(8)
+            }
+            
             VStack(alignment: .leading) {
                 Text(recipe.name)
                     .font(.headline)
-                Text(recipe.category)
-                    .font(.subheadline)
             }
         }
     }
 }
 
 struct RecipeDetailView: View {
-    var recipe: Recipe
+    var recipe: Meal
 
     var body: some View {
         VStack {
-            Image(recipe.imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
+            if let imageUrl = URL(string: recipe.image) {
+                AsyncImage(url: imageUrl) { image in
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                } placeholder: {
+                    Color.gray
+                }
+            } else {
+                Color.gray
+            }
+            
             Text(recipe.name)
                 .font(.title)
                 .padding()
-            Text(recipe.instructions)
+            Text(recipe.idMeal)
                 .multilineTextAlignment(.leading)
                 .padding()
             Spacer()
@@ -54,19 +104,20 @@ struct RecipeDetailView: View {
     }
 }
 
-struct Recipe: Identifiable {
-    let id = UUID()
-    let name: String
-    let category: String
-    let imageName: String
-    let instructions: String
+struct Meal: Identifiable, Decodable {
+    let idMeal: String
+    let strMeal: String
+    let strMealThumb: String
+
+    var id: String { idMeal }
+    var name: String { strMeal }
+    var image: String { strMealThumb }
 }
 
-let dummyRecipes: [Recipe] = [
-    Recipe(name: "Apple Pie", category: "Dessert", imageName: "apple_pie", instructions: "This is how you make an apple pie"),
-    Recipe(name: "Chocolate Cake", category: "Dessert", imageName: "chocolate_cake", instructions: "This is how you make a chocolate cake"),
-    Recipe(name: "Vanilla Cupcakes", category: "Dessert", imageName: "vanilla_cupcakes", instructions: "This is how you make vanilla cupcakes")
-]
+
+struct MealsResponse: Decodable {
+    let meals: [Meal]
+}
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {

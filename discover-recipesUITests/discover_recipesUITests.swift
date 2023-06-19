952b5fe795d3ae6fbc635677715b1a17d56e24a0
Jorge Morin <jmorin1@hawk.iit.edu>
2023-06-19 15:13:05 -0500

    Finished UI Tests

--- discover-recipesUITests/discover_recipesUITests.swift
+++ discover-recipesUITests/discover_recipesUITests.swift
@@ -1,34 +1,96 @@
 import XCTest
 
-final class discover_recipesUITests: XCTestCase {
+class discover_recipesUITests: XCTestCase {
 
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // UI tests must launch the application that they test.
+    func testAppLaunch() throws {
         let app = XCUIApplication()
         app.launch()
 
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(app.navigationBars["Discover Recipes"].exists)
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
+    func testRecipeSelection() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        let fetchExpectation = expectation(description: "Fetch meals")
+        wait(for: [fetchExpectation], timeout: 10)
+
+        let tableExists = NSPredicate(format: "exists == true")
+        let tableVisible = NSPredicate(format: "isHittable == true")
+        let tableQuery = app.tables.element
+        let tableVisibleExpectation = expectation(for: NSCompoundPredicate(andPredicateWithSubpredicates: [tableExists, tableVisible]), evaluatedWith: tableQuery, handler: nil)
+        wait(for: [tableVisibleExpectation], timeout: 5)
+
+        let firstRecipeCell = app.tables.cells.firstMatch
+
+        if firstRecipeCell.exists && firstRecipeCell.isHittable {
+            firstRecipeCell.tap()
+            XCTAssertTrue(app.navigationBars["Recipe Detail"].exists)
         }
     }
+
+
+
+
+
+    override func wait(for expectations: [XCTestExpectation], timeout: TimeInterval) {
+        let waitForExpectations = XCTestExpectation(description: "Wait for expectations")
+        let timer = Timer.scheduledTimer(withTimeInterval: timeout, repeats: false) { _ in
+            waitForExpectations.fulfill()
+        }
+        XCTWaiter().wait(for: expectations + [waitForExpectations], timeout: timeout)
+        timer.invalidate()
+    }
+
+
+
+    func testRecipeDetailContent() throws {
+        let app = XCUIApplication()
+        app.launch()
+        
+        let fetchExpectation = expectation(description: "Fetch meals")
+        wait(for: [fetchExpectation], timeout: 10)
+
+        let tableExists = NSPredicate(format: "exists == true")
+        let tableVisible = NSPredicate(format: "isHittable == true")
+        let tableQuery = app.tables.element
+        let tableVisibleExpectation = expectation(for: NSCompoundPredicate(andPredicateWithSubpredicates: [tableExists, tableVisible]), evaluatedWith: tableQuery, handler: nil)
+        wait(for: [tableVisibleExpectation], timeout: 5)
+
+        let firstRecipeCell = app.tables.cells.element(boundBy: 0)
+        if firstRecipeCell.waitForExistence(timeout: 5) {
+            firstRecipeCell.tap()
+        } else {
+            return
+        }
+
+        XCTAssertTrue(app.navigationBars["Recipe Detail"].exists)
+
+        let recipeNameExists = NSPredicate(format: "exists == true")
+        let recipeNameExpectation = expectation(for: recipeNameExists, evaluatedWith: app.staticTexts["Recipe Name"], handler: nil)
+        wait(for: [recipeNameExpectation], timeout: 5)
+
+        XCTAssertTrue(app.staticTexts["Recipe Name"].exists)
+
+        XCTAssertTrue(app.images["Recipe Image"].exists)
+
+        XCTAssertFalse(app.staticTexts["Loading recipe..."].exists)
+
+        XCTAssertTrue(app.staticTexts["Ingredients"].exists)
+        XCTAssertTrue(app.tables.cells.count > 0)
+
+        XCTAssertTrue(app.staticTexts["Instructions"].exists)
+        XCTAssertTrue(app.staticTexts["Recipe Instructions"].exists)
+    }
+
+
+
 }
